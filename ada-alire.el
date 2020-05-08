0035991fdc7d4e264bf0c8fd04343ff492ac9543
Momozor <skelic3@gmail.com>
2020-05-09 04:17:55 +0800

    Add more functions

--- ada-alire.el
+++ ada-alire.el
@@ -15,3 +15,60 @@
 ;; Full documentation is available as an Info manual.
 
 ;;; Code:
+
+(defvar *ada-project-root* nil)
+
+(defun alr-in-root-directory? () nil)
+
+(defun alr-version () nil)
+
+(defun alr-print-current-project-root-path ()
+  (interactive)
+  (message *ada-project-root*))
+
+(defun alr-cd-project-root ()
+  (interactive)
+  (let ((directory-path
+         (read-directory-name "Project root absolute path: ")))
+    (cd directory-path)
+    (setf *ada-project-root* directory-path)
+    (message "You are now in %s" directory-path)))
+
+(defun alr-build () nil)
+
+(defun alr-run () nil)
+
+(defun alr-search-installable () nil)
+
+(defun alr-with ()
+  (interactive)
+  (let ((crate-name
+         (read-string "Crate name: ")))
+    (cd "/home/momozor/quicklisp/local-projects/non_lisp/ada/nzping")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    (shell-command (format "alr with %s" crate-name))))
+
+  (defun alr-update () nil)
+
+  (defun alr-info (number)
+    "Multiply NUMBER by seven."
+    (interactive "p")
+    (message "The result is %d" (* 8 number)))
+
+;;; ada-alire.el ends here
