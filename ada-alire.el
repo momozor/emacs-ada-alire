19a5399c2b2d571d817e42a9a88fd649bd10e863
Momozor <skelic3@gmail.com>
2020-05-09 00:45:35 +0800

    Initial commit

---
+++ ada-alire.el
@@ -0,0 +1,17 @@
+;;; ada-alire.el --- A lightweight Ada Alire's alr commander.
+
+;; Copyright (C) 2020 Momozor <skelic3@gmail.com, momozor4@gmail.com>
+
+;; Author: Momozor <skelic3@gmail.com, momozor4@gmail.com>
+;; Version: 0.1
+;; Keywords: ada, alire, alr, productivity
+;; URL: https://github.com/momozor/ada-alire
+
+;;; Commentary:
+
+;; Ada Alire is a thin command wrapper run certain alr
+;; commands without switching to terminal (more focus on project development).
+
+;; Full documentation is available as an Info manual.
+
+;;; Code:
